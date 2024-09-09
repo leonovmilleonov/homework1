873f3965757f284c016141195697e01c243f1e31
Viktor Leonov <vitaleonov13112003@gmail.com>
2024-09-09 18:45:14 +0200

    done

--- exercises/src/Exercise1.java
+++ exercises/src/Exercise1.java
@@ -1,13 +1,20 @@
 import org.w3c.dom.ls.LSOutput;
 
+/**
+ * Задание 1
+ * В переменной minutes лежит число от 0 до 59.
+ * Написать функцию, которая принимает в качестве параметра значение переменной minutes
+ * и выводит на консоль в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+ * Протестировать функцию в main.
+ */
 public class Exercise1 {
+    private static final int QUARTER = 1;
     public static void main(String[] args) {
         printMinutes(59);
     }
 
     public static void printMinutes(int minutes) {
-        System.out.println("Minutes: " + minutes);
-        int quarter = (minutes / 15) + 1;
+        int quarter = (minutes / 15) + QUARTER;
         System.out.println("Quarter: " + quarter);
     }
 }
--- exercises/src/Exercise2.java
+++ exercises/src/Exercise2.java
@@ -1,3 +1,20 @@
+/**
+Задание 2
+
+Даны 3 переменные:
+
+- operand1 (double)
+- operand2 (double)
+- operation (char ‘+’, ‘-’, ‘*’, ‘/’, ‘%’)
+
+Написать функцию, которая принимает в качестве параметров эти три переменные и возвращает результат операции.
+Протестировать функцию в main.
+
+Например:
+Параметры: operand1 = 24.4, operand2 = 10.1, operation = ‘+’
+Результат: 34.5 (24.4 + 10.1)
+ */
+
 public class Exercise2 {
     public static void main(String[] args) {
         double a = 10;
